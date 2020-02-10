dc7c4e1a3b70016452315b87f165594afa818bbd
StlMaris123 <stellamarisnjage@gmail.com>
2020-02-10 09:15:04 +0200

    display from to the user

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+
+const App = () => {
+    return (
+        <div className="container" style= {{margin: '10px'}}>
+            <SearchBar />
+        </div>
+    );
+};
+
+export default App;
