a310a96f438f0c9a7b81c9be875d0af4d67ba066
StlMaris123 <stellamarisnjage@gmail.com>
2020-02-17 22:15:52 +0200

    access doms using refs

---
+++ src/components/ImageCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+    constructor(props){
+        super(props)
+        this.imageRef = React.createRef();
+    }
+
+    render () {
+        const {description, urls} = this.props.image;
+        return(
+            <div>
+                <img 
+                  alt={description}
+                  src={urls.regular}
+                  ref={this.imageRef}
+                />
+            </div>
+        )
+    }
+}
+
+export default ImageCard;
